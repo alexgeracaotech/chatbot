0f12d6d20a50d08394d2f9ffba19fcc75227b633
Alex Bessa <alexbessa.dev@gmail.com>
2025-10-16 17:30:51 -0300

    add state message

--- src/components/ChatMessages.js
+++ src/components/ChatMessages.js
@@ -1,22 +1,5 @@
 
 const ChatMessages = ({ newMessage }) => {
-  const messages = [{
-    message: 'Hello!',
-    sender: 'user',
-    id: crypto.randomUUID()
-  }, {
-    message: 'Hello! How can I help you?',
-    sender: 'robot',
-    id: crypto.randomUUID()
-  }, {
-    message: 'Flip a coin.',
-    sender: 'user',
-    id: crypto.randomUUID()
-  }, {
-    message: 'Sure! You got tails!',
-    sender: 'robot',
-    id: crypto.randomUUID()
-  }];
 
   const chatMessageComponent = newMessage.map((chatMessage) => {
     return (
